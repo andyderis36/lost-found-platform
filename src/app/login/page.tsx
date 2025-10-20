e8c3cfa5aed7a5f984d1b09101b926be3245a6c6
Andyderis P.A.S <andyderis36@gmail.com>
2025-10-20 21:11:18 +0800

    Fix ESLint errors for Vercel deployment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -103,7 +103,7 @@ export default function LoginPage() {
           {/* Register Link */}
           <div className="mt-6 text-center">
             <p className="text-gray-600">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link href="/register" className="text-blue-600 font-semibold hover:underline">
                 Create one here
               </Link>
